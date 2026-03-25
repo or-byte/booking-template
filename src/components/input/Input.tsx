15c9e8feb7e3e79c0921c052b08947763a601505
czarbdizon-hub <czarbdizon@gmail.com>
2026-03-25 14:02:01 +0800

    Update as per review

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -6,12 +6,12 @@ type InputProps = {
 } & JSX.InputHTMLAttributes<HTMLInputElement>;
 
 export default function Input(props: InputProps) {
-  const { label, class: className, ...rest } = props;
+  const { label, class: className = "", ...rest } = props;
   return (
     <div class="flex flex-col gap-1 rounded-[10px] px-4 pt-3 pb-2 border border-slate-200 w-full">
       <label class="text-xs font-semibold text-left">{label}</label>
       <input
-        class={`bg-transparent outline-none placeholder:text-slate-400 text-sm pb-1 w-full ${className ?? ""}`}
+        class={`bg-transparent outline-none placeholder:text-slate-400 text-sm pb-1 w-full ${className}`}
         {...rest}
       />
     </div>
