48c6a74c7fe520bc546decede897b6b572423fa3
Ark Lumacad <arkclumacad@gmail.com>
2026-03-25 17:59:14 +0800

    Add product and category modification (#13)
    - Add product and category modification

--- src/lib/category.ts
+++ src/lib/category.ts
@@ -1,12 +1,44 @@
 import { Category as PrismaCategory } from "@prisma/client";
 import prisma from "./prisma";
 
-export type Category = PrismaCategory;
+// Explicitly added count as field in Category to surpass unrecognizable error for _count
+export type Category = PrismaCategory & { count?: number };
 
-export const getCategories = async () => {
-    return await prisma.category.findMany({
-        include: {
-            _count: { select: { products: true } }
-        }
-    });
+export type CategoryFormData = {
+  name: string
+  description: string
+}
+
+/// getCategories fetches all except Room
+// Room is a category but it is used separately
+export const getCategories = async (): Promise<Category[]> => {
+  "use server"
+  const categories = await prisma.category.findMany({
+    where: { name: { not: "Room" } },
+    include: {
+      _count: { select: { products: true } }
+    }
+  });
+
+  return categories.map(mapCategory);
+}
+
+export const createNewCategory = async (form: CategoryFormData): Promise<Category> => {
+  "use server"
+
+  const category = await prisma.category.create({
+    data: {
+      name: form.name,
+      description: form.description
+    }
+  })
+
+  return category;
+}
+
+export function mapCategory(category: PrismaCategory) : Category {
+  return {
+    ...category,
+    count: category._count
+  };
 }
--- src/lib/product.ts
+++ src/lib/product.ts
@@ -2,38 +2,82 @@ import { Product as PrismaProduct } from "@prisma/client"
 import prisma from "./prisma";
 
 export type Product = Omit<PrismaProduct, "price"> & { price: number };
+
 export type ProductFormData = {
-    sku: string;
-    name: string;
-    description: string;
-    price: number;
-    categoryId: number;
+  sku: string;
+  name: string;
+  description: string;
+  price: number;
+  categoryId: number;
 }
 
-export const getAllProducts = async (): Promise<Product[]> => {
-    "use server"
-    const products = await prisma.product.findMany({
-        where: {
-            category: {
-                name: {
-                    not: "Room",
-                },
-            },
-        },
-    });
+export type EditableProduct = Partial<Product> & { id?: number; categoryId?: number }
 
-    return products.map(p => ({
-        ...p,
-        price: p.price.toNumber()
-    }))
+export const getAllProducts = async (): Promise<Product[]> => {
+  "use server"
+
+  const products = await prisma.product.findMany({
+    where: {
+      category: {
+        name: {
+          not: "Room",
+        },
+      },
+    },
+  });
+
+  return products.map(mapProduct);
 }
 
 export const getProductsByCategory = async (id: number): Promise<Product[]> => {
-    "use server";
-    const products = await prisma.product.findMany({ where: { categoryId: id } });
+  "use server"
 
-    return products.map(p => ({
-        ...p,
-        price: p.price.toNumber(),
-    }));
+  const products = await prisma.product.findMany({ where: { categoryId: id } });
+
+  return products.map(mapProduct);
 };
+
+export const createNewProduct = async (form: ProductFormData): Promise<Product> => {
+  "use server"
+
+  const product = await prisma.product.create({
+    data: {
+      name: form.name,
+      sku: form.sku,
+      description: form.description,
+      price: form.price,
+      categoryId: form.categoryId,
+    }
+  });
+  return mapProduct(product);
+}
+
+export const updateProduct = async (form: ProductFormData & { id: number }): Promise<Product> => {
+  "use server"
+
+  const product = await prisma.product.update({
+    where: { id: form.id },
+    data: {
+      name: form.name,
+      sku: form.sku,
+      description: form.description,
+      price: form.price,
+      categoryId: form.categoryId,
+    },
+  });
+
+  return mapProduct(product);
+};
+
+export const deleteProduct = async (id: number) => {
+  "use server"
+
+  await prisma.product.delete({ where: { id } });
+};
+
+export function mapProduct(product: PrismaProduct): Product {
+  return {
+    ...product,
+    price: product.price.toNumber()
+  };
+}
