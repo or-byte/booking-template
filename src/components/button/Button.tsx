e4067ca2e20342255ede7adca98a724716270451
Ark Lumacad <arkclumacad@gmail.com>
2026-04-13 14:06:52 +0800

    Add package pagination (#28)
    - Added package pagination
    - Fixed button prop

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,15 +1,13 @@
-import type { JSX } from "solid-js";
+import { splitProps, JSX } from "solid-js";
 
-type ButtonProps = {
-    children: JSX.Element;
-    class?: string;
-} & JSX.ButtonHTMLAttributes<HTMLButtonElement>;
+type ButtonProps = JSX.ButtonHTMLAttributes<HTMLButtonElement>;
 
 export default function Button(props: ButtonProps) {
-    const { children, class: className, ...rest } = props;
-    return (
-        <button {...rest} class={className}>
-            {children}
-        </button>
-    );
+  const [local, rest] = splitProps(props, ["children", "class"]);
+
+  return (
+    <button {...rest} class={local.class}>
+      {local.children}
+    </button>
+  );
 }
